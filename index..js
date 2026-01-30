56c7bfab04dc2113f0881bf49b468fc6b4269812
Roberto <robertoleao1212@gmail.com>
2026-01-30 09:03:22 -0300

    projeto  portifolio - palestra para surdos

---
+++ index..js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+  document.getElementById("textoSobre").innerText =
+    "Estou aprendendo programação web com HTML, CSS e JavaScript!";
+}
+
+function clicouProjeto(numero) {
+  alert("Projeto " + numero + " clicado!");
+}
